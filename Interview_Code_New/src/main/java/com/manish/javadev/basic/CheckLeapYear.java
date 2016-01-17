8cfe9adffeb8a164760b0dfad76dd7e5fa619dc5
Manish Srivastava <manishjavadevhelp@gmail.com>
2016-01-17 16:53:57 +0530

    Leap Year Concept added

--- Interview_Code_New/src/main/java/com/manish/javadev/basic/CheckLeapYear.java
+++ Interview_Code_New/src/main/java/com/manish/javadev/basic/CheckLeapYear.java
@@ -1,4 +1,5 @@
 package com.manish.javadev.basic;
+
 import java.util.Scanner;
 
 /**
@@ -6,11 +7,17 @@ import java.util.Scanner;
  *
  */
 /**
- * THis program simply checking entered year is leap year or not
- *
+ * This program simply checking entered year is leap year or not.
+ * A leap year is a year containing an extra days. 
+ * It has 366 days instead of 365 days The extra days added in February
+ * which is of 29 days instead of 28 days.
+ * A leap year occurs in every 4 years for
+ * ex 2008 is leap year so next leap year will be 2012 
+ * Except that every 100 year for 100 year special rule will apply
+ * 
  */
 public class CheckLeapYear {
-	
+
 	public static void main(String[] args) {
 		int year = 0;
 		System.out.println("Enter a year for checking leap year");
@@ -19,8 +26,7 @@ public class CheckLeapYear {
 		if (year % 100 == 0)
 			if (year % 400 == 0) {
 				System.out.println(year + " is leap year");
-			} 
-			else {
+			} else {
 				System.out.println(year + " is not a leap year");
 			}
 		else {
