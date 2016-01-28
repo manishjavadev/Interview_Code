9508fb915c2a49c61e2d7b3090f26bbff01caed6
Manish Srivastava <manishjavadevhelp@gmail.com>
2016-01-28 06:42:42 +0530

    Merge Sort using Recursion

---
+++ Interview_Code_New/src/main/java/com/manish/javadev/sorting/MergeSort.java
@@ -0,0 +1,48 @@
+package com.manish.javadev.sorting;
+
+import java.util.Arrays;
+
+public class MergeSort {
+	int arr[] = { 5, 4, 3, 1, 2, 6 };
+	int temp[] = new int[arr.length];
+
+	public static void main(String[] args) {
+		MergeSort ms = new MergeSort();
+		ms.doSorting();
+
+	}
+
+	private void doSorting() {
+		int len = arr.length - 1;
+		merge_sort(0, len);
+		System.out.println("Main Array " + Arrays.toString(arr));
+		System.out.println("Temp Array " + Arrays.toString(temp));
+	}
+
+	private void merge_sort(int low, int high) {
+		if (low != high) {
+			int mid = (low + high) / 2;
+			merge_sort(low, mid);
+			merge_sort(low, mid);
+			mergePartOfArray(low, mid, high);
+		}
+	}
+
+	private void mergePartOfArray(int low, int mid, int high) {
+		int i = low;
+		int j = mid + 1;
+		int k = 0;
+		while ((i <= mid) && (j <= high)) {
+			if (arr[i] < arr[j])
+				temp[k++] = arr[i++];
+			else
+				temp[k++] = arr[j++];
+		}
+		while (i <= mid) {
+			temp[k++] = arr[i++];
+		}
+		while (j <= high) {
+			temp[k++] = arr[j++];
+		}
+	}
+}
