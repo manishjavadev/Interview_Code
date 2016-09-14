8a0461cec2b049ea9d450267bb64e326e0f47d7d
Manish Srivastava <msmanish95@gmail.com>
2016-09-14 11:49:36 +0530

    Swap Node in linked list

--- Interview_Code_New/src/main/java/com/manish/javadev/geeks/linkedlist/DefaultLinkedList.java
+++ Interview_Code_New/src/main/java/com/manish/javadev/geeks/linkedlist/DefaultLinkedList.java
@@ -7,9 +7,11 @@ public class DefaultLinkedList {
 		Entity entity2 = new Entity(20);
 		Entity entity3 = new Entity(30);
 		Entity entity4 = new Entity(40);
+		Entity entity5 = new Entity(50);
 		entity1.next = entity2;
 		entity2.next = entity3;
 		entity3.next = entity4;
+		entity4.next = entity5;
 		return entity1;
 
 	}
---
+++ Interview_Code_New/src/main/java/com/manish/javadev/geeks/linkedlist/SwapNodeInLinkedList.java
@@ -0,0 +1,65 @@
+package com.manish.javadev.geeks.linkedlist;
+
+public class SwapNodeInLinkedList {
+	Entity entity = DefaultLinkedList.getDefaultLinkedList();
+
+	public static void main(String[] args) {
+		SwapNodeInLinkedList swapNodeList = new SwapNodeInLinkedList();
+		swapNodeList.doSwap();
+	}
+
+	private void doSwap() {
+		System.out.println("Before Swap");
+		printList();
+		int first = 20;
+		int second = 40;
+		System.out.println("\nWe are going to swap " + first + "  and "
+				+ second);
+		swapNode(20, 40);
+		System.out.println("\nAfter Swap");
+		printList();
+
+	}
+
+	public void printList() {
+		Entity tNode = entity;
+		while (tNode != null) {
+			System.out.print(tNode.data + " ");
+			tNode = tNode.next;
+		}
+	}
+
+	private void swapNode(int first, int second) {
+		Entity prevx = null;
+		Entity prevy = null;
+
+		Entity currx = entity;
+		Entity curry = entity;
+		if (first == second) {
+			return;
+		}
+		while (currx != null && currx.data != first) {
+			prevx = currx;
+			currx = currx.next;
+		}
+		while (curry != null && curry.data != second) {
+			prevy = curry;
+			curry = curry.next;
+		}
+		if (prevx != null) {
+			prevx.next = curry;
+		} else {
+			entity = curry;
+		}
+		if (prevy != null) {
+			prevy.next = currx;
+		} else {
+			entity = currx;
+		}
+		Entity temEntity = currx.next;
+		currx.next = curry.next;
+		curry.next = temEntity;
+
+	}
+
+}
