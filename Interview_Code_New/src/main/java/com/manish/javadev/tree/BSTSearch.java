8f26cbf7ac067359ca46b640f8f0fbe65c6b93bf
kmamani <kmamani@KMAMANI-IN.in.oracle.com>
2019-08-29 21:57:52 +0530

    code changes

--- Interview_Code_New/src/main/java/com/manish/javadev/tree/BSTSearch.java
+++ Interview_Code_New/src/main/java/com/manish/javadev/tree/BSTSearch.java
@@ -2,57 +2,53 @@ package com.manish.javadev.tree;
 
 public class BSTSearch {
 	static BSTNode start;
-	static BSTNode current;
+	static BSTNode crr;
 
 	public static void main(String[] args) {
 		BSTSearch bstSearch = new BSTSearch();
 		BSTNode root = bstSearch.findWithOutRecursion(BSTDefaultTree2.getDefaultTree(), 45);
-		if (root == null) {
-			System.out.println("requested data is not there");
-		} else {
-			System.out.println(root.data);
-		}
+		displayNodeData(root);
 
 		root = bstSearch.findWithRecursion(BSTDefaultTree.getDefaultTree(), 45);
-		if (root == null) {
-			System.out.println("requested data is not there");
-		} else {
-			System.out.println(root.data);
-		}
+		displayNodeData(root);
 
 	}
 
-	private BSTNode findWithOutRecursion(BSTNode root, int data) {
-		current = root;
-		if (current == null) {
-			return current;
+	private static void displayNodeData(BSTNode root) {
+		if (root == null) {
+			System.out.println("requested data is not there");
+		} else {
+			System.out.println(root.data);
 		}
-		while (current != null) {
-			if (current.data == data) {
-				return current;
+	}
+
+	private BSTNode findWithOutRecursion(BSTNode root, int data) {
+		crr = root;
+		while (crr != null) {
+			if (crr.data == data) {
+				return crr;
 			}
-			if (current.data > data) {
-				current = current.left;
+			if (crr.data > data) {
+				crr = crr.left;
 			} else {
-				current = current.right;
+				crr = crr.right;
 			}
 		}
 		return null;
 	}
 
 	private BSTNode findWithRecursion(BSTNode root, int data) {
-
-		current = root;
-		if (current == null) {
-			return current;
+		crr = root;
+		if (crr == null) {
+			return crr;
 		}
-		if (current.data == data) {
-			return current;
+		if (crr.data == data) {
+			return crr;
 		}
-		if (current.data > data) {
-			return findWithRecursion(current.left, data);
+		if (crr.data > data) {
+			return findWithRecursion(crr.left, data);
 		} else {
-			return findWithRecursion(current.right, data);
+			return findWithRecursion(crr.right, data);
 		}
 	}
 }
