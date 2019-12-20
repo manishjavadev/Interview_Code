e04b7b7b6f166c445229686687932ff3296c2408
kmamani <kmamani@KMAMANI-IN.in.oracle.com>
2019-12-20 19:54:39 +0530

    Char Count Using For Loop and Map

---
+++ Interview_Code_New/src/main/java/com/manish/javadev/geeks/array/CharCountUsingForLoopAndMap.java
@@ -0,0 +1,27 @@
+package com.manish.javadev.geeks.array;
+
+import java.util.HashMap;
+
+public class CharCountUsingForLoopAndMap {
+
+	static char arr[] = { 'a', 'b', 'c', 'd', 'c', 'c', 'd', 'd', 'd' };
+	static HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+
+	public static void main(String[] args) {
+		CharCountUsingForLoopAndMap ffd = new CharCountUsingForLoopAndMap();
+		ffd.getDuplicateCount(arr);
+		System.out.println(map);
+	}
+
+	private void getDuplicateCount(char[] ch) {
+		for (int low = 0; low < ch.length; low++) {
+			if (map.containsKey(arr[low])) {
+				map.put(arr[low], map.get(arr[low]) + 1);
+			} else {
+				map.put(arr[low], 1);
+			}
+		}
+
+	}
+
+}
