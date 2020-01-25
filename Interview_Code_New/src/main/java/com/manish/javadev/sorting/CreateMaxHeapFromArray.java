7e88cf12bfe99f3f3f7df3cf42a781e7a5863729
kmamani <kmamani@KMAMANI-IN.in.oracle.com>
2020-01-25 18:53:50 +0530

    Build Max Heap Code Added

--- Interview_Code_New/src/main/java/com/manish/javadev/sorting/CreateMaxHeapFromArray.java
+++ Interview_Code_New/src/main/java/com/manish/javadev/sorting/CreateMaxHeapFromArray.java
@@ -7,36 +7,51 @@ import java.util.Arrays;
  *
  */
 public class CreateMaxHeapFromArray {
-	static int[] arr = { 10, 20, 30, 40, 50 };
-
 	public static void main(String[] args) {
-
-		for (int i = 0; i < arr.length; i++) {
-			build_Max_Heap(arr[i], i);
-		}
-		display(arr);
-	}
-
-	private static void display(int[] arr2) {
+		int arr[] = { 11, 12, 13, 5, 6, 7, 14 };
+		// int arr[] = { 12, 11, 13, 5, 6, 7, 8, 4, 3 };
+		CreateMaxHeapFromArray hs = new CreateMaxHeapFromArray();
+		hs.max_heap(arr);
+		System.out.println("Sorted array is");
 		System.out.println(Arrays.toString(arr));
 	}
 
-	private static void build_Max_Heap(int num, int location) {
-		int parent = 0;
-		while (location > 0) {
-			// Find parent for the current location
-			parent = (location - 1) / 2;
-			// Here need to check parent is greater than number
-			// if yes: arr[location] replace with number
-			if (num <= arr[parent]) {
-				arr[location] = num;
-				return;
-			}
-			arr[location] = arr[parent];
-			// After each swap location should refer corresponding parent
-			location = parent;
-		}
-		arr[0] = num;
+	public void max_heap(int arr[]) {
+		int n = arr.length;
+
+		// Build heap (rearrange array)
+		for (int i = n / 2 - 1; i >= 0; i--)
+			build_maxheap(arr, n, i);
+
 	}
 
+	// To heapify a subtree rooted with node i which is
+	// an index in arr[]. n is size of heap
+	void build_maxheap(int arr[], int size, int curr) {
+		int largest = curr; // Initialize largest as root
+		int l = 2 * curr + 1; // left = 2*i + 1
+		int r = 2 * curr + 2; // right = 2*i + 2
+
+		// If left child is larger than root
+		if (l < size && arr[l] > arr[largest])
+			largest = l;
+
+		// If right child is larger than largest so far
+		if (r < size && arr[r] > arr[largest])
+			largest = r;
+
+		// If largest is not root
+		if (largest != curr) {
+			swapNode(arr, largest, curr);
+			// Recursively heapify the affected sub-tree
+			build_maxheap(arr, size, largest);
+		}
+	}
+
+	private void swapNode(int[] arr, int start, int left) {
+		int temp;
+		temp = arr[start];
+		arr[start] = arr[left];
+		arr[left] = temp;
+	}
 }
