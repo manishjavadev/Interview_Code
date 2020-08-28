69068f42d44785e916f13e904e7f1b9d620a9756
kmamani <kmamani@KMAMANI-IN.in.oracle.com>
2020-08-28 12:38:00 +0530

    Code Chnages

--- Interview_Code_New/src/main/java/com/manish/javadev/sorting/HeapSort.java
+++ Interview_Code_New/src/main/java/com/manish/javadev/sorting/HeapSort.java
@@ -5,47 +5,48 @@ import java.util.Arrays;
 public class HeapSort {
 
 	public static void main(String[] args) {
-		int[] array = { 48, 32, 46, 29, 10 };
+		// int[] array = { 48, 32, 46, 29, 10 };
+		// int array[] = { 11, 12, 13, 5, 6, 7, 14 };
+		int array[] = { 72, 64, 65, 56, 32, 46, 54, 29, 48 };
 		HeapSort hs = new HeapSort();
 		hs.heapSort(array);
 	}
 
 	private void heapSort(int[] array) {
-		for (int i = array.length - 1; i > 0; i--) {
+		for (int i = array.length - 1; i >= 0; i--) {
 			swapNode(array, i, 0);
 			heapify(array, i, 0);
 		}
 		System.out.println(Arrays.toString(array));
 	}
 
-	private void heapify(int[] array, int n, int crr) {
-		int large = crr;
-		int left = 2 * large + 1;
-		int right = 2 * large + 2;
+	private void heapify(int[] array, int size, int crr) {
+		int left = 2 * crr + 1;
+		int right = 2 * crr + 2;
 
-		while (right < n) {
-			if (array[large] >= array[left] && array[large] >= array[right]) {
+		while (right < size) {
+			if (array[crr] >= array[left] && array[crr] >= array[right]) {
 				return;
 			}
 			if (array[right] <= array[left]) {
-				swapNode(array, large, left);
-				large = left;
+				swapNode(array, crr, left);
+				crr = left;
 			} else {
-				swapNode(array, large, right);
-				large = right;
+				swapNode(array, crr, right);
+				crr = right;
 			}
-			left = 2 * large + 1;
-			right = 2 * large + 2;
+			left = 2 * crr + 1;
+			right = 2 * crr + 2;
 		}
-		if (left == n - 1 && array[large] < array[left]) {
-			swapNode(array, large, left);
+		if (left == size - 1 && array[crr] < array[left]) {
+			swapNode(array, crr, left);
 		}
 	}
 
-	private void swapNode(int[] array, int start, int left) {
+	private void swapNode(int[] array, int start, int last) {
 		int temp;
 		temp = array[start];
-		array[start] = array[left];
-		array[left] = temp;
+		array[start] = array[last];
+		array[last] = temp;
 	}
 }
