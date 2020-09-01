b202dd191470f7a7a48a5d242d1adfaeb51ef97b
kmamani <kmamani@KMAMANI-IN.in.oracle.com>
2020-09-01 21:06:34 +0530

    Tree def Added

--- Interview_Code_New/src/main/java/com/manish/javadev/tree/TreeOrBSTInfo.java
+++ Interview_Code_New/src/main/java/com/manish/javadev/tree/TreeOrBSTInfo.java
@@ -4,4 +4,64 @@ public class TreeOrBSTInfo {
 	/**
 	 * 
 	 */
+	/**
+	 * Complete Binary:
+	 * 
+	 * Tree A full binary tree (sometimes proper binary tree or 2-tree) is a tree in
+	 * which every node other than the leaves has two children. A complete binary
+	 * tree is a binary tree in which every level, except possibly the last, is
+	 * completely filled, and all nodes are as far left as possible.
+	 */
+
+	/**
+	 * Full Binary Tree:
+	 * 
+	 * A Binary Tree is a full binary tree if every node has 0 or 2 children. The
+	 * following are the examples of a full binary tree. We can also say a full
+	 * binary tree is a binary tree in which all nodes except leaf nodes have two
+	 * children.
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 		  18
+           /       \  
+         15         30  
+        /  \        /  \
+      40    50    100   40
+
+             18
+           /    \   
+         15     20    
+        /  \       
+      40    50   
+    /   \
+   30   50
+
+               18
+            /     \  
+          40       30  
+                   /  \
+                 100   40
+	 */
+	
+	
+	/**
+	 * Perfect Binary:
+	 * 
+	 * Tree A Binary tree is a Perfect Binary Tree in which all the internal nodes
+	 * have two children and all leaf nodes are at the same level. The following are
+	 * the examples of Perfect Binary Trees.
+	 * 
+	 *        18
+           /       \  
+         15         30  
+        /  \        /  \
+      40    50    100   40
+
+
+               18
+           /       \  
+         15         30  
+	 */
 }
